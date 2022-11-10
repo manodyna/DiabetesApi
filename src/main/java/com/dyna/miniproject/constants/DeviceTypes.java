6122eb406c3fe8e0451729c273048add2e6e69b7
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-11-10 20:29:25 +0530

    enums set up

---
+++ src/main/java/com/dyna/miniproject/constants/DeviceTypes.java
@@ -0,0 +1,12 @@
+package com.dyna.miniproject.constants;
+
+public enum DeviceTypes {
+    DEVICETYPE1("Device1"),
+    DEVICETYPE2("Device2");
+
+    public final String device;
+
+    DeviceTypes(String device) {
+        this.device = device;
+    }
+}
